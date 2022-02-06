d358e7ae9f6ee727ec8d32fc45d11792b51f3566
Melis <tyn.melis.ru@gmail.com>
2022-02-06 21:35:04 +0300

    Js debug

--- js/main.js
+++ js/main.js
@@ -10,20 +10,36 @@ const options = {
     type: "custom",
     method: function (value, item) {
       return `
-        <span style="color: gray">${value}</span>
-        <span style="color: gray">${item.capital}</span>
+      <span style="color: gray">${value}</span>
+      <span style="color: gray">${item.capital}</span>
       `;
     }
   }
 };
 $('.form__country').easyAutocomplete(options);
 
-let formVal = $(`.content__notes>form`).on(`submit`, function (e) {
-  e.preventDefault();
-  let formСountry = $(`.form__country`).val()
-  let formDate = $(`.form__date`).val()
-  let reviewMassage = $(`.review__massage`).val()
-  let div = $(`<div></div>`).text(formСountry, formDate, reviewMassage)
-  $(`sidebar__content`).append(div)
-  
-})
+// console.log(mainForm);
+// console.log(formClass);
+
+
+$(`.form`).on(`submit`, function (e) {
+  e.preventDefault(e);
+  let formInputCountry = $(`#form__country`);
+  let formInputdate = $(`.form__date`);
+  let formInputTextarea = $(`.review__massage`);
+  let div = $(`<div>`).addClass(`sidebar__content`);
+
+  let spanInputCountry = $(`<span></span>`).text(formInputCountry.val()); // Место
+  let spanInputdate = $(`<span></span>`).text(formInputdate.val()); // Дата
+  let divInputTextarea = $(`<div></div>`).text(formInputTextarea.val()); // Massage
+
+  div.append(spanInputCountry)
+  div.append(spanInputdate)
+  div.append(divInputTextarea)
+  $(`div.sidebar`).append(div)
+});
+// let formVal = $(`.content__notes>form`).on(`submit`, function (e) {
+//   e.preventDefault();
+//   let formVal = document.forms
+//   console.log(formVal);
+// })
