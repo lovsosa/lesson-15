9fe857de36a3e5bcede502785fcf7b7df724ee91
Melis <tyn.melis.ru@gmail.com>
2022-02-07 14:26:41 +0300

    fin

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// подсказки поиска
 const options = {
   url: "https://restcountries.com/v2/all?fields=name,flag,capital,population",
   getValue: "name",
@@ -10,20 +11,68 @@ const options = {
     type: "custom",
     method: function (value, item) {
       return `
-        <span style="color: gray">${value}</span>
-        <span style="color: gray">${item.capital}</span>
+      <span style="color: gray">${value}</span>
+      <span style="color: gray">${item.capital}</span>
       `;
     }
   }
 };
 $('.form__country').easyAutocomplete(options);
-
-let formVal = $(`.content__notes>form`).on(`submit`, function (e) {
-  e.preventDefault();
-  let formСountry = $(`.form__country`).val()
-  let formDate = $(`.form__date`).val()
-  let reviewMassage = $(`.review__massage`).val()
-  let div = $(`<div></div>`).text(formСountry, formDate, reviewMassage)
-  $(`sidebar__content`).append(div)
+let data = [];
+// событие формы
+$(`.form`).on(`submit`, function (e) {
+  e.preventDefault(e);
+  let formInputCountry = $(`.form__country`);
+  let formInputdate = $(`.form__date`);
+  let formInputTextarea = $(`.review__massage`);
   
-})
+
+  let item = {
+    counrty: formInputCountry.val(),
+    date: formInputdate.val(),
+    text: formInputTextarea.val()
+  }
+  data.push(item);
+  add(item, data.length)
+
+});
+$(`.sidebar`).on(`click`, function (event) {
+  if (event.target.classList.contains(`cross`)) {
+    event.target.parentElement.parentElement.remove()
+    
+  }
+});
+function add(item, count) {
+  let cross = $(`<img src="../img/cross.png" alt="#">`).addClass(`cross`)
+  let spanText = $(`<p></p>`).text(`Удалить запись`).addClass(`del--notes`)
+  let divsidebar = $(`<div>`).addClass(`sidebar__content`);
+  let div = $(`<div>`).addClass(`content__item`)
+  let topick = $(`<div>`).addClass(`content__topick`);
+
+  
+  let spanInputCountry = $(`<span></span>`).addClass(`country__text`).text(`Место пробывание: ` + item.counrty); // Место
+  let spanInputdate = $(`<span></span>`).addClass(`date__text`).text(item.date); // Дата
+  let divInputTextarea = $(`<div></div>`).addClass(`textarea__text`).text(item.text); // Massage
+
+  topick.prepend(`Запись #` + count)
+  topick.append(spanInputdate)
+  topick.append(spanInputCountry)
+  topick.append(spanText)
+  topick.append(cross)
+  div.append(divInputTextarea)
+  divsidebar.append(topick)
+  divsidebar.append(div)
+  $(`div.sidebar`).append(divsidebar)
+  
+}
+
+// фокус
+const mainForm = document.forms.main;
+const mainFormInput = mainForm.nameInput;
+const mainFormInputPlaceholder = mainFormInput.placeholder;
+mainFormInput.addEventListener("focus", function () {
+  mainFormInput.placeholder = "";
+});
+mainFormInput.addEventListener("blur", function () {
+mainFormInput.placeholder = mainFormInputPlaceholder;
+});
